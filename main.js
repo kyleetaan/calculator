042aee51a6b8d672ec7e9c4b06bcfbee3b3e982c
Kyle <tan.kyleemmanuel@gmail.com>
2022-01-14 19:29:37 +0800

    working calculation

---
+++ main.js
@@ -0,0 +1,86 @@
+const inputScreen = document.getElementById("inputScreen");
+const calculation = document.getElementById("calculationScreen");
+const deci = document.getElementById("decimal");
+const oprtr = document.querySelectorAll(".operator");
+const equal = document.getElementById("equals");
+
+oprtr.forEach((button) =>
+  button.addEventListener('click', () => setOperation(button.textContent))
+);
+
+let currentOperator = "";
+let currentNumber = "";
+let nextNumber = "";
+let result = "";
+
+
+deci.addEventListener("onclick", decimal);
+equal.addEventListener("click", operate);
+
+function input(number){
+    if(check()){
+        inputScreen.textContent += number;
+    } 
+    else {
+        console.log("false");
+    }
+}
+
+function setOperation(operation) {
+    currentOperator = operation;
+    currentNumber = Number(inputScreen.textContent);
+    calculation.textContent = `${currentNumber} ${currentOperator}`
+    inputScreen.textContent = "0";
+}
+
+//not yet implemented
+function decimal() {
+    if(inputScreen.textContent === "0" || !(inputScreen.textContent.contains("."))){
+        inputScreen.textContent += ".";
+    }
+}
+
+function check(){
+    if(inputScreen.textContent === "0"){
+        inputScreen.textContent = "";
+        return true;
+    }
+
+    return true;
+
+}
+
+function clearScreen(){ 
+    
+    inputScreen.textContent = "0";
+    calculation.textContent = "";
+}
+
+function operate(){
+    nextNumber = Number(inputScreen.textContent);
+    
+    switch(currentOperator){
+        case "+":
+            result = currentNumber + nextNumber;
+            changeScreen(result);
+            break;
+        case "-":
+            result = currentNumber - nextNumber;
+            changeScreen(result);
+            break;
+        case "*":
+            result = currentNumber * nextNumber;
+            changeScreen(result);
+            break;
+        case "/":
+            result = currentNumber / nextNumber;
+            changeScreen(result);
+            break;
+    }
+}
+
+function changeScreen(result){
+    calculation.textContent = "";
+    calculation.textContent = `${currentNumber} ${currentOperator} ${inputScreen.textContent}`;
+    inputScreen.textContent = result;
+}
